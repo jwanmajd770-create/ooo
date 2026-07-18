5ebbef5198399545eb126209550e252ee250e0a1
majed132010 <majed132010@gmail.com>
2026-07-18 23:43:34 +0300

    fix: allow all CORS origins

--- backend/server.py
+++ backend/server.py
@@ -1107,8 +1107,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', 'https://ooo-five-teal.vercel.app').split(','),
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
