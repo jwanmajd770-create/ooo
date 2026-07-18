b8dfe6a86c16614d3f27dc6a130ce1201dea0939
majed132010 <majed132010@gmail.com>
2026-07-18 23:38:46 +0300

    fix: update CORS default origin

--- backend/server.py
+++ backend/server.py
@@ -1108,7 +1108,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=os.environ.get('CORS_ORIGINS', 'https://ooo-five-teal.vercel.app').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
